feebec47867dbda73b32fcbf153dc45a790b2848
Steven McCanne <steve@mccanne.com>
2020-12-28 18:53:43 -0800

    add code

---
+++ go.mod
@@ -0,0 +1,7 @@
+module zmarshal
+
+go 1.15
+
+require github.com/brimsec/zq v0.26.0
+
+replace github.com/brimsec/zq => github.com/brimsec/zq v0.26.1-0.20201225202527-d739c0744d4f
---
+++ zmarshal.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/brimsec/zq/zng/resolver"
+	"github.com/brimsec/zq/zson"
+)
+
+type Thing interface {
+	Color() string
+}
+
+type Plant struct {
+	MyColor string
+}
+
+func (p *Plant) Color() string { return p.MyColor }
+
+type Animal struct {
+	MyColor string
+}
+
+func (a *Animal) Color() string { return a.MyColor }
+
+func Make(which string) Thing {
+	if which == "rose" {
+		return &Plant{"red"}
+	}
+	if which == "ivy" {
+		return &Plant{"green"}
+	}
+	if which == "flamingo" {
+		return &Animal{"pink"}
+	}
+	return nil
+}
+
+func ex1() {
+	rose := Make("rose")
+	flamingo := Make("flamingo")
+	m := zson.NewMarshaler()
+	m.Decorate(zson.StyleSimple)
+	roseZSON, _ := m.Marshal(rose)
+	fmt.Println(roseZSON)
+	flamingoZSON, _ := m.Marshal(flamingo)
+	fmt.Println(flamingoZSON)
+}
+
+func ex2() {
+	f := Make("flamingo")
+	m := zson.NewMarshaler()
+	m.Decorate(zson.StyleSimple)
+	flamingoZSON, _ := m.Marshal(f)
+
+	u := zson.NewUnmarshaler()
+	u.Bind(Animal{}, Plant{})
+	var flamingo Thing
+	err := u.Unmarshal(flamingoZSON, &flamingo)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("The flamingo is " + flamingo.Color())
+	}
+}
+
+func ex3() {
+	f := Make("flamingo")
+	m := zson.NewMarshaler()
+	m.Decorate(zson.StyleSimple)
+	flamingoZSON, _ := m.Marshal(f)
+
+	u := zson.NewUnmarshaler()
+	u.Bind(Animal{}, Plant{})
+	var flamingo Thing
+	u.Unmarshal(flamingoZSON, &flamingo)
+	_, ok := flamingo.(*Animal)
+	fmt.Printf("The flamingo is an Animal? %t\n", ok)
+}
+
+func ex4() {
+	rose := Make("rose")
+	flamingo := Make("flamingo")
+	m := zson.NewMarshaler()
+	m.Decorate(zson.StylePackage)
+	roseZSON, _ := m.Marshal(rose)
+	fmt.Println(roseZSON)
+	flamingoZSON, _ := m.Marshal(flamingo)
+	fmt.Println(flamingoZSON)
+}
+
+func ex5() {
+	rose := Make("rose")
+	flamingo := Make("flamingo")
+	m := zson.NewMarshaler()
+	m.NamedBindings([]resolver.Binding{{"Plant.v0", Plant{}}, {"Animal.v0", Animal{}}})
+	roseZSON, _ := m.Marshal(rose)
+	fmt.Println(roseZSON)
+	flamingoZSON, _ := m.Marshal(flamingo)
+	fmt.Println(flamingoZSON)
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		usage()
+	}
+	ex, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		usage()
+	}
+	switch ex {
+	case 1:
+		ex1()
+	case 2:
+		ex2()
+	case 3:
+		ex3()
+	case 4:
+		ex4()
+	case 5:
+		ex5()
+	default:
+		usage()
+	}
+}
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "usage: zmarshal example-number")
+	os.Exit(1)
+}
